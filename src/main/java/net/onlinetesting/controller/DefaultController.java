80688e630991cdc3ef2afbd05b45bec7c8e92fe0
illya <illya.tkach.dev@gmail.com>
2019-10-08 17:12:09 +0300

    add ratings

--- src/main/java/net/onlinetesting/controller/DefaultController.java
+++ src/main/java/net/onlinetesting/controller/DefaultController.java
@@ -1,26 +1,11 @@
 package net.onlinetesting.controller;
 
-import net.onlinetesting.model.Test;
-import net.onlinetesting.model.TestRating;
-import net.onlinetesting.model.User;
-import net.onlinetesting.repository.TestRepository;
-import net.onlinetesting.repository.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import java.util.Set;
-
 @Controller
 public class DefaultController {
-
-    @Autowired
-    TestRepository testRepository;
-
-    @Autowired
-    UserRepository userRepository;
-
 
     @GetMapping("/")
     public String indexPage(Model model) {
--- src/main/java/net/onlinetesting/model/Answer.java
+++ src/main/java/net/onlinetesting/model/Answer.java
@@ -2,7 +2,6 @@ package net.onlinetesting.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.*;
-
 import javax.persistence.*;
 
 @Getter
